c258a0ec6d273a82ac94dfd52ca9c8e9d91b118e
Douglas Henrique <doughrqe@gmail.com>
2020-02-11 18:14:41 -0300

    requisitos ate 3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { BrowserRouter, Router, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Router>
-          <div>Movie Card Library CRUD</div>
-        </Router>
+        <div>Movie Card Library CRUD</div>
+        <Switch>
+          <Route exact path="movies/:id" component={MovieDetails} />
+          <Route exact path="movies/:id/edit" component={EditMovie} />
+          <Route exact path="/movies/new" component={NewMovie} />
+          <Route path="" component={NotFound} />
+          <Route exact path="/" component={MovieList} />
+        </Switch>
       </Switch>
     </BrowserRouter>
   );
