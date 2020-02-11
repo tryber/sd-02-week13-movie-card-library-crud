c4dfc5621ea70ce92dd735e17a3f38048a1326c9
Douglas Henrique <doughrqe@gmail.com>
2020-02-11 18:02:12 -0300

    Segundo Requisito

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Router } from 'react-router-dom';
+import { BrowserRouter, Router, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <Router>
-        <div>Movie Card Library CRUD</div>
-      </Router>
+      <Switch>
+        <Router>
+          <div>Movie Card Library CRUD</div>
+        </Router>
+      </Switch>
     </BrowserRouter>
   );
 }
