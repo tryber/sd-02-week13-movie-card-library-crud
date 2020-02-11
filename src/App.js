fd41a347af3b337df90d250e74bb492ed36a558f
Douglas Henrique <doughrqe@gmail.com>
2020-02-11 18:01:23 -0300

    Primeiro requisito

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Router } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div>Movie Card Library CRUD</div>
-    </Router>
+    <BrowserRouter>
+      <Router>
+        <div>Movie Card Library CRUD</div>
+      </Router>
+    </BrowserRouter>
   );
 }
 
