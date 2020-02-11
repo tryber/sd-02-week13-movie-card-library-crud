8c03332de813c34385cdf3d9953351264d2d793b
Douglas Henrique <doughrqe@gmail.com>
2020-02-11 19:33:00 -0300

    Code Climate

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
-import { Link } from 'react-router-dom';
+
 
 class MovieDetails extends Component {
   constructor(props) {
