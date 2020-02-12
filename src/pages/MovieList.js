af1e42f6498bc68faf222f18f5b1b982ba6abad7
Giorgia <giorgialeoniac@gmail.com>
2020-02-12 19:38:53 -0300

    Correções no código by ESLINT que eu simplesmente não vi

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -14,18 +14,17 @@ class MovieList extends Component {
 
   componentDidMount() {
     movieAPI.getMovies()
-      .then((data) => this.SetState ({
+      .then((data) => this.SetState({
         movies: data,
         loading: false,
       }));
-  };
+  }
 
 
   render() {
     const { movies, loading } = this.state;
     // Render Loading here if the request is still happening
-    if loading return <Loading />
-
+    if (loading) return <Loading />;
     return (
       <div className="movie-list">
         {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
