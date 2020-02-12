f69b96c5a9f7f7531734a789acf8dc6d8c66a187
Miguel Miranda <ayandram7727@gmail.com>
2020-02-12 18:57:30 -0200

    Feito até o requisito 10 - Ao submeter o formulário, EditMovie deve atualizar o filme e redirecionar a página para a rota raiz

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
 import { MovieForm, Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
@@ -10,21 +11,27 @@ class EditMovie extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(updatedMovie) {
-  }
-
   componentDidMount() {
     movieAPI.getMovie(this.props.match.params.id)
       .then((data) => this.setState({ movie: data, status: 'success' }));
+  }
+
+  handleSubmit(updatedMovie) {
+    movieAPI.updateMovie(updatedMovie)
+      .then(() => this.setState({ shouldRedirect: true }));
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
       // Redirect
+      return (
+        <Redirect to="/" />
+      );
     }
 
     if (status === 'loading') {
+      // Loading
       return (
         <Loading />
       );
