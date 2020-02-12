ffa94711a3b7c7787c82ceafbdf6cfa532529e84
guicgs <etohlif@protonmail.com>
2020-02-12 15:16:13 -0300

    requisito 2 cumprido

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { BrowserRouter, Switch } from 'react-router-dom';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
+      <Switch>
+
+      </Switch>
       <div>Movie Card Library CRUD</div>
     </BrowserRouter>
   );
