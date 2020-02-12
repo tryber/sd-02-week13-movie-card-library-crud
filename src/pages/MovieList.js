9d01d67cab06b39c63059905cd572145ec0098f2
guicgs <etohlif@protonmail.com>
2020-02-12 16:29:47 -0300

    requisito 4 concluído

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -4,10 +4,24 @@ import MovieCard from '../components/MovieCard';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movies: '',
+    };
+  }
+
+  componentDidMount() {
+    movieAPI.getMovies()
+      .then((response) => this.setState({ movies: response }));
+  }
+
   render() {
     const { movies } = this.state;
 
     // Render Loading here if the request is still happening
+    if (!movies) return <h2>Carregando...</h2>;
 
     return (
       <div className="movie-list">
