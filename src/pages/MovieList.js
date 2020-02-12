716b3918f8b2cd0e144eb06e4704470e708501f9
Giorgia <giorgialeoniac@gmail.com>
2020-02-12 18:17:41 -0300

    Começando MovieList, requisito 4

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Loading from '../components/Loading';
 import MovieCard from '../components/MovieCard';
+
 
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: '',
+      loading: true,
+    };
+  }
+
+
   render() {
     const { movies } = this.state;
 
