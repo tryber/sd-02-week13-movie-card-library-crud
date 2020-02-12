7ae2be5c0aa2c575f6a8fee7ab48cfa6e11ea585
Miguel Miranda <ayandram7727@gmail.com>
2020-02-12 01:44:05 -0200

    MovieList alterado para atender requisito 4 - Ao ser montado, MovieList deve fazer uma requisição para buscar a lista de filmes a ser renderizada

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { movies: [], loading: true };
+  }
+
+  componentDidMount() {
+    movieAPI.getMovies()
+      .then(data => this.setState({ movies: data, loading: false }))
+  }
+
   render() {
     const { movies } = this.state;
 
     // Render Loading here if the request is still happening
+    if (this.state.loading) {
+      return <Loading />
+    }
 
     return (
       <div className="movie-list">
