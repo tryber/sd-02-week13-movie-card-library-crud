70c6a2780da9834b9baa754eeb4914ef49ff11b0
Douglas Henrique <doughrqe@gmail.com>
2020-02-12 15:50:25 -0300

    Remoção de linhas desnecessarias

--- src/App.js
+++ src/App.js
@@ -14,21 +14,8 @@ function App() {
         <Switch>
           <Route exact path="/" component={MovieList} />
           <Route exact path="/movies/:id" component={MovieDetails} />
-          <Route
-            exact
-            path="/movies"
-            render={(props) => (
-              <MovieDetails {...props} />
-            )}
-          />
           <Route path="/movies/new" component={NewMovie} />
-          <Route path="/movies/:id/edit" component={EditMovie} />
-          <Route
-            path="/:id/edit"
-            render={(props) => (
-              <EditMovie {...props} />
-            )}
-          />
+          <Route path="/:id/edit" component={EditMovie} />
           <Route component={NotFound} />
         </Switch>
       </BrowserRouter>
