d59c0452b850f225b83dfc340aa896a0fe864da9
Miguel Miranda <ayandram7727@gmail.com>
2020-02-13 11:42:47 -0200

    Tentando passar no CC para finalizar o requisito 13

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -170,21 +170,4 @@ class MovieForm extends React.Component {
   }
 }
 
-MovieForm.propTypes = {
-  movie: PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired,
-    subtitle: PropTypes.string.isRequired,
-    storyline: PropTypes.string.isRequired,
-    rating: PropTypes.number.isRequired,
-    imagePath: PropTypes.string.isRequired,
-    genre: PropTypes.string.isRequired,
-  }),
-  onSubmit: PropTypes.func.isRequired,
-};
-
-MovieForm.defaultProps = {
-  movie: {},
-}
-
 export default MovieForm;
