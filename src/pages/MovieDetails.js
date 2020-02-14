74e44b34b80beeb5823591a511b95821f590468a
lauro <laurolyra@gmail.com>
2020-02-14 18:33:29 -0300

    adição de propTypes

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
@@ -55,3 +56,5 @@ class MovieDetails extends Component {
 }
 
 export default MovieDetails;
+
+MovieDetails.propTypes = { match: PropTypes.string.isRequired };
