e5a295d6fd74830402f5de8c0e12be30649b67ae
phtf92 <phtf92@gmail.com>
2020-02-18 13:46:09 -0300

    Melhorando exact nas rotas

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ function App() {
     <Router>
       <Switch>
         <Route exact path="/" component={MovieList} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
         <Route exact path="/movies/new" component={NewMovie} />
-        <Route exact path="/movies/:id/edit" component={EditMovie} />
-        <Route exact path="/movies/:id" component={MovieDetails} />
+        <Route path="/movies/:id" component={MovieDetails} />
         <Route component={Notfound} />
       </Switch>
     </Router>
