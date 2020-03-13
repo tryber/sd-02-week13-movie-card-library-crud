578bdd1eef0eae4ac2f091807970a9377a09839f
Douglas Henrique <doughrqe@gmail.com>
2020-03-13 19:13:43 -0300

    CC

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Loading.css';
 
-const Loading = () => <div className="loading">Carregando...</div>
+const Loading = () => <div className="loading">Carregando...</div>;
 
 export default Loading;
